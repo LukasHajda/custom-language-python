617ea23ad7a2f0370fce95c7afa987dd3a62701b
Lukas Hajda <hajda.lukas21@gmail.com>
2024-03-03 12:30:17 +0100

    Refactor, small fix (scanner)

--- classes/scanner.py
+++ classes/scanner.py
@@ -3,30 +3,41 @@ from typing import Callable, Any
 from classes.token import Token, TokenVariant
 from ply.lex import LexToken
 from ply import lex
+from classes.errors import UnexpectedTokenException
 
 SOURCE = 'source_code.txt'
 NEWLINE = '\n'
 
+RESERVED_WORDS = {
+    'if': TokenVariant.T_IF,
+    'while': TokenVariant.T_WHILE,
+    'else': TokenVariant.T_ELSE,
+    'null': TokenVariant.T_NULL,
+    'false': TokenVariant.T_BOOLEAN,
+    'true': TokenVariant.T_BOOLEAN,
+    'is': TokenVariant.T_ASSIGN
+}
+
 TOKENS = {
     'plus': TokenVariant.T_PLUS, 'minus': TokenVariant.T_MINUS,
     'division': TokenVariant.T_DIVISION, 'multiplication': TokenVariant.T_MULTIPLICATION,
-    'assign': TokenVariant.T_ASSIGN,
+
     'less': TokenVariant.T_LESS, 'less_equal': TokenVariant.T_LESS_EQUAL,
     'greater': TokenVariant.T_GREATER, 'greater_equal': TokenVariant.T_GREATER_EQUAL,
     'equal': TokenVariant.T_EQUAL, 'not_equal': TokenVariant.T_NOT_EQUAL,
 
     'integer': TokenVariant.T_INTEGER, 'float': TokenVariant.T_FLOAT,
-    'boolean': TokenVariant.T_BOOLEAN, 'null': TokenVariant.T_NULL,
     'eof': TokenVariant.T_EOF,
 
-    'identifier': TokenVariant.T_IDENTIFIER, 'if': TokenVariant.T_IF,
-    'else': TokenVariant.T_ELSE, 'while': TokenVariant.T_WHILE,
+    'identifier': TokenVariant.T_IDENTIFIER,
+
     'left_p': TokenVariant.T_LEFT_P, 'right_p': TokenVariant.T_RIGHT_P,
     'left_curly_p': TokenVariant.T_LEFT_CURLY_P, 'right_curly_p': TokenVariant.T_RIGHT_CURLY_P,
     'dot': TokenVariant.T_DOT,
 
     'ignore_whitespace': TokenVariant.T_WHITESPACE, 'ignore_comments': TokenVariant.T_COMMENT,
-    'ignore_newline': TokenVariant.T_NEWLINE
+    'ignore_newline': TokenVariant.T_NEWLINE,
+
 }
 
 
@@ -42,19 +53,19 @@ def update_position(function) -> Callable:
             self.total += len(token.value)
             self.column = self.total - len(token.value) + 1
 
-        if 'ignore' not in token.type:
-            return token
+        return function(self, token)
     return wrapper
 
 
 class Scanner:
     tokens = (
         *(list(TOKENS.keys())),
+        *(list(set(RESERVED_WORDS.keys())))
     )
 
     def __init__(self):
         self.scanner = lex.lex(module = self)
-        self.row = 0
+        self.row = 1
         self.column = 1
         self.total = 0
         self.__set_text()
@@ -119,11 +130,6 @@ class Scanner:
         return token
 
     @update_position
-    def t_assign(self, token: LexToken) -> LexToken:
-        r"""[iI][sS]"""
-        return token
-
-    @update_position
     def t_less_equal(self, token: LexToken) -> LexToken:
         r"""\<\="""
         return token
@@ -169,42 +175,30 @@ class Scanner:
         return token
 
     @update_position
-    def t_null(self, token: LexToken) -> LexToken:
-        r"""[Nn][Uu][Ll][Ll]"""
-        return token
-
-    @update_position
-    def t_boolean(self, token: LexToken) -> LexToken:
-        r"""([Ff][Aa][Ll][Ss][Ee]]|[Tt][Rr][Uu][Ee])"""
-        return token
-
-    @update_position
-    def t_if(self, token: LexToken) -> LexToken:
-        r"""[Ii][Ff]"""
-        return token
-
-    @update_position
-    def t_else(self, token: LexToken) -> LexToken:
-        r"""[Ee][Ll][Ss][Ee]"""
-        return token
-
-    @update_position
     def t_identifier(self, token: LexToken) -> LexToken:
         r"""[a-zA-Z_][a-z]+"""
+        token.type = token.value if token.value in RESERVED_WORDS else 'identifier'
         return token
+
+    def t_error(self, token: LexToken) -> None:
+        token.lexer.skip(1)
+        raise UnexpectedTokenException(
+            message = "Unexpected character: '{token}' at line {row} and column {column}".format(
+                token = token.value,
+                row = self.row,
+                column = self.column
+            )
+        )
 
     def __set_text(self) -> None:
         with open(SOURCE, 'r') as file:
             self.scanner.input(file.read())
         file.close()
 
-
-    # TODO: t_error dokoncit.
-
-    def get_token(self):
+    def get_token(self) -> Token:
         lex_token = self.scanner.token()
         return Token(
-            token_variant = TOKENS.get(lex_token.type),
+            token_variant = TOKENS.get(lex_token.type, RESERVED_WORDS.get(lex_token.type)),
             value = lex_token.value,
             row = self.row,
             column = self.column
--- classes/token.py
+++ classes/token.py
@@ -2,56 +2,55 @@ from enum import Enum
 
 
 class TokenVariant(Enum):
-    T_PROGRAM = 1,
+    T_PROGRAM = 'program',
 
     # END OF EXPRESSION
-    T_DOT = 2,
+    T_DOT = 'dot',
 
     # OPERATORS
-    T_ASSIGN = 4,
-    T_PLUS = 5,
-    T_MINUS = 6,
-    T_DIVISION = 7,
-    T_MULTIPLICATION = 8,
-    T_LESS = 9,
-    T_LESS_EQUAL = 10,
-    T_GREATER = 11,
-    T_GREATER_EQUAL = 12,
-    T_EQUAL = 13,
-    T_NOT_EQUAL = 14,
+    T_ASSIGN = 'assign',
+    T_PLUS = 'plus',
+    T_MINUS = 'minus',
+    T_DIVISION = 'division',
+    T_MULTIPLICATION = 'multiplication',
+    T_LESS = 'less',
+    T_LESS_EQUAL = 'less_equal',
+    T_GREATER = 'greater',
+    T_GREATER_EQUAL = 'greater_equal',
+    T_EQUAL = 'equal',
+    T_NOT_EQUAL = 'not_equal',
 
     # TYPES
-    T_INTEGER = 15,
-    T_FLOAT = 16,
-    T_STRING = 17,
-    T_BOOLEAN = 18,
-    T_NULL = 19,
-    T_EOF = 20,
+    T_INTEGER = 'integer',
+    T_FLOAT = 'float',
+    T_STRING = 'string',
+    T_BOOLEAN = 'boolean',
+    T_NULL = 'null',
+    T_EOF = 'eof',
 
     # RESERVED WORDS
-    T_IDENTIFIER = 21,
-    T_FUNCTION = 22,
-    T_NEGATE = 23,
-    T_IF = 24,
-    T_ELSE = 25,
-    T_WHILE = 26
+    T_IDENTIFIER = 'identifier',
+    T_FUNCTION = 'function',
+    T_NEGATE = 'negate',
+    T_IF = 'if',
+    T_ELSE = 'else',
+    T_WHILE = 'while'
 
     # OTHERS
-    T_LEFT_P = 27,
-    T_RIGHT_P = 28,
-    T_LEFT_CURLY_P = 29,
-    T_RIGHT_CURLY_P = 30,
+    T_LEFT_P = 'left_p',
+    T_RIGHT_P = 'right_p',
+    T_LEFT_CURLY_P = 'left_curly_p',
+    T_RIGHT_CURLY_P = 'right_curly_p',
 
     # Errors
-    T_ERROR = 31,
+    T_ERROR = 'error',
 
     # INITIALIZATION
-    T_EMPTY = 32,
+    T_EMPTY = 'empty',
 
-    T_COMMENT = 33,
-    T_WHITESPACE = 34
-    T_NEWLINE = 35
-
+    T_COMMENT = 'comment',
+    T_WHITESPACE = 'whitespace'
+    T_NEWLINE = 'newline'
 
 
 class Token:

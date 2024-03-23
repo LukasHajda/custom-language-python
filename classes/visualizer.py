70780c33cd738992edd27be0baa27d6db5f19bdf
Lukas Hajda <hajda.lukas21@gmail.com>
2024-03-23 10:26:23 +0100

    Small refactor, update gitignore (updates)

--- classes/visualizer.py
+++ classes/visualizer.py
@@ -7,8 +7,6 @@ class Visualizer(graphviz.Digraph, VisitorVisualizer):
     def __init__(self, root: Program):
         super().__init__()
         self.root: Program = root
-        self.stack: deque = deque([root])
-        self.parents: deque = deque([None])
         self.nodes: dict = {
             Program.__name__: 0,
             AssignmentStatement.__name__: 0,
@@ -23,23 +21,14 @@ class Visualizer(graphviz.Digraph, VisitorVisualizer):
             Block.__name__: 0
         }
 
-    def __increase_queues(self, stack_value: Any, parent_value: Any) -> None:
-        self.stack.append(stack_value)
-        self.parents.append(parent_value)
-
     def __traverse_ast(self) -> None:
-        while self.stack:
-            current_node = self.stack.popleft()
-            parent = self.parents.popleft()
-
-            self.add(current_node, parent = parent)
+        self.add(self.root, parent = None)
 
     def __generate_node_name(self, node: ASTnode, extra_info: str = '') -> (str, str):
         node_group_count: int = self.nodes.get(str(node))
         id_name = f"{node}_{str(node_group_count + 1)}"
         label_name = f"{node} {extra_info}"
         self.nodes[str(node)] += 1
-
         return id_name, label_name
 
     def __add_node(self, node: ASTnode, extra_info: str = '') -> str:
@@ -52,14 +41,14 @@ class Visualizer(graphviz.Digraph, VisitorVisualizer):
 
     def add_program(self, node: Program, _: str) -> None:
         current_node_name = self.__add_node(node)
-        self.__increase_queues(stack_value = node.block, parent_value = current_node_name)
+        self.add(node.block, parent = current_node_name)
 
     def add_assignment_statement(self, node: AssignmentStatement, parent: str) -> None:
         current_node_name = self.__add_node(node)
         self.edge(parent, current_node_name)
 
-        self.__increase_queues(stack_value = node.name, parent_value = current_node_name)
-        self.__increase_queues(stack_value = node.value, parent_value = current_node_name)
+        self.add(node.name, parent = current_node_name)
+        self.add(node.value, parent = current_node_name)
 
     def add_variable(self, node: Variable, parent: str) -> None:
         current_node_name = self.__add_node(
@@ -81,8 +70,8 @@ class Visualizer(graphviz.Digraph, VisitorVisualizer):
             extra_info = f"operator: {node.operator.value[1]}"
         )
         self.edge(parent, current_node_name)
-        self.__increase_queues(stack_value = node.left_operand, parent_value = current_node_name)
-        self.__increase_queues(stack_value = node.right_operand, parent_value = current_node_name)
+        self.add(node.left_operand, parent = current_node_name)
+        self.add(node.right_operand, parent = current_node_name)
 
     def add_unary_operation(self, node: UnaryOperation, parent: str) -> None:
         current_node_name = self.__add_node(
@@ -90,41 +79,45 @@ class Visualizer(graphviz.Digraph, VisitorVisualizer):
             extra_info = f"operator: {node.operator.value}"
         )
         self.edge(parent, current_node_name)
-        self.__increase_queues(stack_value = node.operand, parent_value = current_node_name)
+
+        self.add(node.operand, parent = current_node_name)
 
     def add_if_statement(self, node: IfStatement, parent: str) -> None:
         current_node_name = self.__add_node(node)
         self.edge(parent, current_node_name)
-        self.__increase_queues(stack_value = node.condition, parent_value = current_node_name)
-        self.__increase_queues(stack_value = node.block, parent_value = current_node_name)
+
+        self.add(node.condition, parent = current_node_name)
+        self.add(node.block, parent = current_node_name)
 
         if node.else_block:
-            self.stack.append(node.else_block)
-            self.parents.append(current_node_name)
+            self.add(node.else_block, parent = current_node_name)
 
     def add_else_statement(self, node: ElseStatement, parent: str) -> None:
         current_node_name = self.__add_node(node)
         self.edge(parent, current_node_name)
-        self.__increase_queues(stack_value = node.block, parent_value = current_node_name)
+
+        self.add(node.block, parent = current_node_name)
 
     def add_while_statement(self, node: WhileStatement, parent: str) -> None:
         current_node_name = self.__add_node(node)
         self.edge(parent, current_node_name)
-        self.__increase_queues(stack_value = node.condition, parent_value = current_node_name)
-        self.__increase_queues(stack_value = node.block, parent_value = current_node_name)
+
+        self.add(node.condition, parent = current_node_name)
+        self.add(node.block, parent = current_node_name)
 
     def add_condition(self, node: Condition, parent: str) -> None:
         current_node_name = self.__add_node(node)
         self.edge(parent, current_node_name)
-        self.__increase_queues(stack_value = node.value, parent_value = current_node_name)
+
+        self.add(node.value, parent = current_node_name)
 
     def add_block(self, node: Block, parent: str) -> None:
         current_node_name = self.__add_node(node)
         self.edge(parent, current_node_name)
 
         for statement in node.statements:
-            self.__increase_queues(stack_value = statement, parent_value = current_node_name)
+            self.add(statement, parent = current_node_name)
 
     def visualize_tree(self) -> None:
         self.__traverse_ast()
-        self.render('graph', view = True)
+        self.render('AST', format = 'pdf', cleanup = True, view = True)
